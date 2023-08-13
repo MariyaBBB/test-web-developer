254ceb87aebac1034a15d8d2c464171f0166272a
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-08-13 21:23:29 +0300

    Добавление валидации на совпадение паролей

--- js/modal.js
+++ js/modal.js
@@ -15,12 +15,12 @@ if (modalLinks.length > 0) {
     }
 }
 
-const modalCloseIcons = document.querySelectorAll('.close-modal');
-if(modalCloseIcons.length > 0) {
-    for(let index = 0; index < modalCloseIcons.length; index++) {
-        const modalCloseIcon = modalCloseIcons[index];
-        modalCloseIcon.addEventListener('click', function(e) {
-            modalClose(modalCloseIcon.closest('.modal'));
+const modalCloseButtons = document.querySelectorAll('.close-modal');
+if(modalCloseButtons.length > 0) {
+    for(let index = 0; index < modalCloseButtons.length; index++) {
+        const modalCloseButton = modalCloseButtons[index];
+        modalCloseButton.addEventListener('click', function(e) {
+            modalClose(modalCloseButton.closest('.modal'));
             e.preventDefault();
         })
     }
@@ -41,6 +41,66 @@ function modalOpen(curentModal) {
     }
 }
 
+const passwordControls = document.querySelectorAll('.password-control');
+const passwordInputs = document.querySelectorAll('.password');
+
+for(let index = 0; index < passwordControls.length; index++) {
+    const passwordControl = passwordControls[index];
+    passwordControl.addEventListener('click', () => {
+        if(passwordInputs[index].type === 'password') {
+            passwordInputs[index].type = 'text';
+            passwordControl.classList.remove('view');
+            passwordControl.classList.add('no-view');
+        } else {
+            passwordInputs[index].type = 'password'
+            passwordControl.classList.remove('no-view');
+            passwordControl.classList.add('view');
+        }
+    })
+}
+
+const saveButton = document.querySelector('.save-button');
+const modalMessage = document.querySelector('.modal-message');
+const modalForm = document.querySelector('.modal-form');
+const messageNoMatchPasswords = document.querySelector('.message-no-match-passwords');
+
+function resetForm() {
+    document.getElementById("name").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("email").value = "";
+
+    for(let index = 0; index < passwordInputs.length; index++) {
+        passwordInputs[index].value = "";
+        passwordInputs[index].classList.remove('no-match-password');
+    }
+
+    messageNoMatchPasswords.style.display = "none";
+    modalMessage.classList.remove('open');
+    modalForm.classList.remove('hidden-modal');
+}
+
+function showMessage() {
+    modalMessage.classList.add('open');
+    modalForm.classList.add('hidden-modal');
+}
+
+function validatePasswords(callback) {
+    if(passwordInputs[0].value === passwordInputs[1].value) {
+        callback();
+    }
+    else {
+        for(let index = 0; index < passwordInputs.length; index++) {
+            passwordInputs[index].classList.add('no-match-password');
+            messageNoMatchPasswords.style.display = 'block';
+        }
+    }
+}
+        
+saveButton.addEventListener('click', function() {
+    validatePasswords(showMessage); 
+});
+
 function modalClose(modalActive) {
-        modalActive.classList.remove('open');
+    modalActive.classList.remove('open');
+    resetForm();
 }
